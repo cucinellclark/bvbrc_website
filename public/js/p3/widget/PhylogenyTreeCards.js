30a1aea75fe37d672336573eba056324fc8569af
Mehmet Kuscuoglu <kscglu@gmail.com>
2026-05-08 10:05:15 -0700

    add action bar for info and download buttons in PhylogenyVirus

--- public/js/p3/widget/PhylogenyTreeCards.js
+++ public/js/p3/widget/PhylogenyTreeCards.js
@@ -565,7 +565,8 @@ define([
         if (typeof this.onSelectTree === 'function') {
           this.onSelectTree({
             url: url, name: name, definition: def,
-            groupTitle: groupTitle || '', section: sectionLabel || ''
+            groupTitle: groupTitle || '', section: sectionLabel || '',
+            metadata: metadata || null
           });
         }
       });
